cea29d90ec2a002142274b79be17b0373d986afd
Rahul Kumar <rahulkumar9199270356@gmail.com>
2026-01-26 16:04:06 +0530

    implement the login ,profile,footer,feed section

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,35 @@
-import React from 'react'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import Login from './components/Login'
-import Body from './components/Body'
-import Profile from './components/Profile'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Body from "./components/Body";
+import Login from "./components/Login";
+import Profile from "./components/Profile";
+import { Provider } from "react-redux";
+import appStore from "./utils/appStore";
+import Feed from "./components/Feed";
+import Connections from "./components/Connections";
+import Requests from "./components/Requests";
+import Premium from "./components/Premium";
+import Chat from "./components/Chat";
 
-const App = () => {
+function App() {
   return (
-  <>
-    <BrowserRouter basename="/">
-  <Routes>
-
-    
-  <Route path="/" element={<Body />}/>
-  <Route path="/login" element={<Login />}/>
-  <Route path="/profile" element={<Profile />}/>
-
-        
-        
-         </Routes>
-      </BrowserRouter>
-      </>
-  )
+    <>
+      <Provider store={appStore}>
+        <BrowserRouter basename="/">
+          <Routes>
+            <Route path="/" element={<Body />}>
+              <Route path="/" element={<Feed />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="/profile" element={<Profile />} />
+              <Route path="/connections" element={<Connections />} />
+              <Route path="/requests" element={<Requests />} />
+              <Route path="/premium" element={<Premium />} />
+              <Route path="/chat/:targetUserId" element={<Chat />} />
+            </Route>
+          </Routes>
+        </BrowserRouter>
+      </Provider>
+    </>
+  );
 }
 
-export default App
+export default App;
