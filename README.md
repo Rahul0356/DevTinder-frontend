d43750313714dc4a35ac23f5b6d48600ac2f703c
Rahul Kumar <rahulkumar9199270356@gmail.com>
2026-01-26 12:03:06 +0530

    basic setup of devtinder frontend

---
+++ README.md
@@ -0,0 +1,4 @@
+Create a Vite + React application
+Remove unecessary code and create a Hello World app
+Install Tailwind CSS
+Install Daisy UI
---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const App = () => {
+  return (
+   <button class="btn btn-primary">Button</button>
+  )
+}
+
+export default App
