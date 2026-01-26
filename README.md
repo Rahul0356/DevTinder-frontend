35ebf241746347924b77a55a355f3a4cc267a381
Rahul Kumar <rahulkumar9199270356@gmail.com>
2026-01-26 13:10:42 +0530

    routing

--- README.md
+++ README.md
@@ -1,4 +1,9 @@
-Create a Vite + React application
-Remove unecessary code and create a Hello World app
-Install Tailwind CSS
-Install Daisy UI
+# DevTinder
+
+- Create a Vite + React application
+- Remove unecessary code and create a Hello World app
+- Install Tailwind CSS
+- Install Daisy UI
+- Add NavBar component to App.jsx
+- Create a NavBar.jsx separate Component file
+- Install react router dom
--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,25 @@
 import React from 'react'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Login from './components/Login'
+import Body from './components/Body'
+import Profile from './components/Profile'
 
 const App = () => {
   return (
-   <button class="btn btn-primary">Button</button>
+  <>
+    <BrowserRouter basename="/">
+  <Routes>
+
+    
+  <Route path="/" element={<Body />}/>
+  <Route path="/login" element={<Login />}/>
+  <Route path="/profile" element={<Profile />}/>
+
+        
+        
+         </Routes>
+      </BrowserRouter>
+      </>
   )
 }
 
